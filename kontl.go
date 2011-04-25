84471aa0009fbd05d52a7c8162f58628abb672fb
Jim Geovedi <jim.geovedi@gmail.com>
2011-04-25 12:59:38 +0700

    minor clean-up.

--- kontl.go
+++ kontl.go
@@ -6,6 +6,7 @@ import (
     "os"
     "flag"
     "http"
+    "log"
     "regexp"
 )
 
@@ -13,30 +14,28 @@ func main() {
     flag.Parse()
 
     if flag.NArg() < 1 {
-        fmt.Fprintln(os.Stderr, "Usage: kontl [URL]")
-        os.Exit(-1)
+        fmt.Fprintf(os.Stderr, "Usage: %s [URL]\n", os.Args[0])
+        os.Exit(1)
     }
 
     u := "http://kon.tl/create.php"
     d := map[string]string{"url": flag.Arg(0)}
 
-    fmt.Println("Long URL:", flag.Arg(0))
+    fmt.Println(" Long URL:", flag.Arg(0))
 
     r, err := http.PostForm(u, d)
     if err != nil {
-        fmt.Println("POST:", err)
-        os.Exit(-1)
+        log.Fatalln(err)
     }
     defer r.Body.Close()
 
     b, err := ioutil.ReadAll(r.Body)
     if err != nil {
-        fmt.Println("POST:", err)
-        os.Exit(-1)
+        log.Fatalln(err)
     }
 
     s := string(b)
-    x := regexp.MustCompile("<a href=(.*) target=_blank")
+    x := regexp.MustCompile("<a href=(.*) target=_blank>")
     m := x.FindStringSubmatch(s)[1]
 
     fmt.Println("Short URL:", m)
